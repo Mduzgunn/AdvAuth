75cf1324f561f6138cb557f2db51d3ec58faafbc
Apocalypse <aduzgun99@gmail.com>
2021-07-07 01:32:31 +0300

    user.html sayfası backend ve frontend kısımları geliştirildi

--- WebParent/BackEnd/src/main/java/com/md/admin/user/UserController.java
+++ WebParent/BackEnd/src/main/java/com/md/admin/user/UserController.java
@@ -1,7 +1,12 @@
 package com.md.admin.user;
 
+import com.md.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -9,6 +14,12 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @GetMapping("/users")
+    public String listAll(Model model){
+        List<User> listUsers = service.listAll();
+        model.addAttribute("listUsers", listUsers);
+        return "users";
+    }
 
 
 
--- WebParent/BackEnd/src/main/java/com/md/admin/user/UserService.java
+++ WebParent/BackEnd/src/main/java/com/md/admin/user/UserService.java
@@ -7,13 +7,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public interface UserService {
-
+public class UserService {
     @Autowired
-    private UserRepository repo;
+    private UserRepository userRepo;
 
     public List<User> listAll(){
-        return (List<User>) repo.findAll();
+        return (List<User>) userRepo.findAll();
     }
-
 }
