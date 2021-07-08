c838b3497a11d889bd003fa0bfd317407bf2a887
Apocalypse <aduzgun99@gmail.com>
2021-07-08 19:14:49 +0300

    updated new user add functionality and added cancel button functions

--- WebParent/BackEnd/src/main/java/com/md/admin/user/UserController.java
+++ WebParent/BackEnd/src/main/java/com/md/admin/user/UserController.java
@@ -1,11 +1,13 @@
 package com.md.admin.user;
 
+import com.md.common.entity.Role;
 import com.md.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -24,15 +26,23 @@ public class UserController {
 
     @GetMapping("/users/new")
     public String newUser(Model model){
+        List<Role> listRoles = service.listRoles();
+
         User user = new User();
+        user.setEnabled(true);
         model.addAttribute("user", user);
+        model.addAttribute("listRoles", listRoles);
+
 
         return "user_form";
     }
 
     @PostMapping("/users/save")
-    public String saveUser(User user){
+    public String saveUser(User user, RedirectAttributes redirectAttributes){
         System.out.println(user);
+        service.save(user);
+
+        redirectAttributes.addFlashAttribute("message", "The user has been saved succesfully.");
         return "redirect:/users";
     }
 
--- WebParent/BackEnd/src/main/java/com/md/admin/user/UserService.java
+++ WebParent/BackEnd/src/main/java/com/md/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.md.admin.user;
 
+import com.md.common.entity.Role;
 import com.md.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,9 +10,20 @@ import java.util.List;
 @Service
 public class UserService {
     @Autowired
-    private UserRepository userRepo;
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     public List<User> listAll(){
-        return (List<User>) userRepo.findAll();
+        return (List<User>) userRepository.findAll();
+    }
+
+    public List<Role> listRoles(){
+        return (List<Role>) roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
     }
 }
