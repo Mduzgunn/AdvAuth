5ebade85e55a277d82beb8657560514fb90349be
Apocalypse <aduzgun99@gmail.com>
2021-08-10 04:53:19 +0300

    Adding an image is under development

--- WebParent/BackEnd/src/main/java/com/md/admin/user/UserController.java
+++ WebParent/BackEnd/src/main/java/com/md/admin/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -40,11 +42,13 @@ public class UserController {
     }
 
     @PostMapping("/users/save")
-    public String saveUser(User user, RedirectAttributes redirectAttributes){
+    public String saveUser(User user, RedirectAttributes redirectAttributes,
+                           @RequestParam("image")MultipartFile multipartFile){
         System.out.println(user);
-        service.save(user);
+        System.out.println(multipartFile.getOriginalFilename());
+        //service.save(user);
 
-        redirectAttributes.addFlashAttribute("message", "The user has been saved succesfully.");
+        //redirectAttributes.addFlashAttribute("message", "The user has been saved succesfully.");
         return "redirect:/users";
     }
 
