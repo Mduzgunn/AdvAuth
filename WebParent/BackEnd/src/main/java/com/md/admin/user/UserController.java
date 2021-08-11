b4826e7753d3e311218dd90b93c487cfd0b33370
Apocalypse <aduzgun99@gmail.com>
2021-08-11 04:25:33 +0300

    save user with photos

--- WebParent/BackEnd/src/main/java/com/md/admin/user/UserController.java
+++ WebParent/BackEnd/src/main/java/com/md/admin/user/UserController.java
@@ -1,10 +1,12 @@
 package com.md.admin.user;
 
+import com.md.admin.FileUploadUtil;
 import com.md.common.entity.Role;
 import com.md.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -21,14 +24,14 @@ public class UserController {
     private UserService service;
 
     @GetMapping("/users")
-    public String listAll(Model model){
+    public String listAll(Model model) {
         List<User> listUsers = service.listAll();
         model.addAttribute("listUsers", listUsers);
         return "users";
     }
 
     @GetMapping("/users/new")
-    public String newUser(Model model){
+    public String newUser(Model model) {
         List<Role> listRoles = service.listRoles();
 
         User user = new User();
@@ -43,18 +46,26 @@ public class UserController {
 
     @PostMapping("/users/save")
     public String saveUser(User user, RedirectAttributes redirectAttributes,
-                           @RequestParam("image")MultipartFile multipartFile){
-        System.out.println(user);
-        System.out.println(multipartFile.getOriginalFilename());
+                           @RequestParam("image") MultipartFile multipartFile) throws IOException {
+        if (!multipartFile.isEmpty()) {
+            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            user.setPhotos(fileName);
+            User savedUser = service.save(user);
+            String uploadDir = "user-photos/" + savedUser.getId();
+
+            FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+        }
+
+
         //service.save(user);
 
-        //redirectAttributes.addFlashAttribute("message", "The user has been saved succesfully.");
+        redirectAttributes.addFlashAttribute("message", "The user has been saved succesfully.");
         return "redirect:/users";
     }
 
     @GetMapping("/users/edit/{id}")
     public String editUser(@PathVariable(name = "id") Integer id, Model model,
-                           RedirectAttributes redirectAttributes){
+                           RedirectAttributes redirectAttributes) {
         try {
             User user = service.get(id);
             List<Role> listRoles = service.listRoles();
@@ -65,18 +76,20 @@ public class UserController {
 
 
             return ("user_form");
-        } catch (UserNotFoundException exception){
+        } catch (UserNotFoundException exception) {
             redirectAttributes.addFlashAttribute("message", exception.getMessage());
             return "redirect:/users";
 
         }
     }
+
     @GetMapping("/users/delete/{id}")
     public String deleteUser(@PathVariable(name = "id") Integer id,
                              Model model,
                              RedirectAttributes redirectAttributes) {
         try {
-            service.delete(id);;
+            service.delete(id);
+            ;
             redirectAttributes.addFlashAttribute("message",
                     "The user ID " + id + " has been deleted successfully");
         } catch (UserNotFoundException ex) {
@@ -88,7 +101,7 @@ public class UserController {
 
     @GetMapping("/users/{id}/enabled/{status}")
     public String updateUserEnabledStatus(@PathVariable("id") Integer id,
-                                          @PathVariable("status") boolean enabled, RedirectAttributes redirectAttributes){
+                                          @PathVariable("status") boolean enabled, RedirectAttributes redirectAttributes) {
         service.updateUserEnabledStatus(id, enabled);
         String status = enabled ? "enabled" : "disabled";
         String message = "The user ID " + id + " has been " + status;
