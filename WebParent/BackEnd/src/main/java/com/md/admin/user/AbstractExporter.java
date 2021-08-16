3246a28d40c6d12cb57f09d0f5f203a2196f330a
Apocalypse <aduzgun99@gmail.com>
2021-08-16 04:59:42 +0300

    export users to excel

---
+++ WebParent/BackEnd/src/main/java/com/md/admin/user/AbstractExporter.java
@@ -0,0 +1,28 @@
+package com.md.admin.user;
+
+import com.md.common.entity.User;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class AbstractExporter {
+
+    public void setResponseHeader(HttpServletResponse httpServletResponse, String contentType, String extension) throws IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String timestamp = dateFormat.format(new Date());
+        String fileName = "users_" + timestamp + extension;
+
+        httpServletResponse.setContentType(contentType);
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=" + fileName;
+        httpServletResponse.setHeader(headerKey,headerValue);
+
+
+
+    }
+}
