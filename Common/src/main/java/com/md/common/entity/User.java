7c3ab973f75b2229a0263aa5cdfe184c99377f91
Apocalypse <aduzgun99@gmail.com>
2021-08-19 07:18:12 +0300

    listing by screen size

--- Common/src/main/java/com/md/common/entity/User.java
+++ Common/src/main/java/com/md/common/entity/User.java
@@ -135,4 +135,8 @@ public class User {
         if (id==null || photos==null) return "/images/default.png";
         return "/user-photos/" + this.id + "/" + this.photos;
     }
+    @Transient
+    public String getFullName(){
+        return firstName + " " + lastName;
+    }
 }
